1f5615c63cca3befee3a29abe230b5432c32c28f
singh janmanpreet <janmanpreet.singh@gmail.com>
2022-10-06 11:45:10 +0200

    correction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import ReactDOM from 'react-dom/client';
+
 function sayHello(name){
     return <h1>hello, {name}!</h1>
 }
+
+let root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(sayHello("Jaman"))
