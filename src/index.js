b517584d796061f37c118020a24f20d058e93193
singh janmanpreet <janmanpreet.singh@gmail.com>
2022-10-06 11:41:38 +0200

    correction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import ReactDOM from 'react-dom/client';
+
 function mySum(a,b){
     return <h2>{a + b}</h2>
 }
+
+let root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(mySum(2,5))
